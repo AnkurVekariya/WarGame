80d4f0ada71fec153a475602c99556dcdf4a0ecc
Ankur <ankurvekariya2@gmail.com>
2024-08-31 17:20:33 +0530

    - created framework project for WarGame SDK

---
+++ WarGameSDK/WarGameSDK/WarGameSDK.h
@@ -0,0 +1,18 @@
+//
+//  WarGameSDK.h
+//  WarGameSDK
+//
+//  Created by Ankur Vekaria on 31/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WarGameSDK.
+FOUNDATION_EXPORT double WarGameSDKVersionNumber;
+
+//! Project version string for WarGameSDK.
+FOUNDATION_EXPORT const unsigned char WarGameSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WarGameSDK/PublicHeader.h>
+
+
