c840b4cb5d9e13447265da5f85ac632afd346bdf
Ankur <ankurvekariya2@gmail.com>
2024-08-31 17:23:30 +0530

    - added WarGame public interface to access sdk

---
+++ WarGameSDK/WarGameSDK/WarGame.swift
@@ -0,0 +1,14 @@
+//
+//  WarGame.swift
+//  WarGameSDK
+//
+//  Created by Ankur Vekaria on 31/08/24.
+//
+
+import Foundation
+import Combine
+
+public class WarGame: ObservableObject {
+    
+    public init() {}
+}
