4da134c4193dc5b9c5dc2d71cd6fe1ad2785466c
Ankur <ankurvekariya2@gmail.com>
2024-08-31 17:26:32 +0530

    - added utility constants for DeckOfCards API base url

---
+++ WarGameSDK/WarGameSDK/SupportingFiles/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WarGameSDK
+//
+//  Created by Ankur Vekaria on 31/08/24.
+//
+
+import Foundation
+
+class Utility {
+    static let baseUrl: String = "https://deckofcardsapi.com/api/deck"
+}
