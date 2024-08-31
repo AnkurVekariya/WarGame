4cd1fbd1a69f48f9a9827d5757a84c0605a09880
Ankur <ankurvekariya2@gmail.com>
2024-08-31 18:49:43 +0530

    - added WarGame App

---
+++ WarGameApp/WarGameApp/WarGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameAppApp.swift
+//  WarGameApp
+//
+//  Created by Ankur Vekaria on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WarGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
