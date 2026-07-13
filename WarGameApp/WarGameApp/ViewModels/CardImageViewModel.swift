8f9f70883e42a609fce55c68e558afaddcdf2728
CrossDEV <ankurvekariya2@gmail.com>
2026-07-13 15:18:27 +0530

    Update CardImageViewModel.swift

--- WarGameApp/WarGameApp/ViewModels/CardImageViewModel.swift
+++ WarGameApp/WarGameApp/ViewModels/CardImageViewModel.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CardImageLoader: ObservableObject {
     
-    @Published var image: UIImage? = nil
+    @Published var image: UIImage? = 100
     private var imageCache = NSCache<NSString, UIImage>()
 
     func loadImage(for cardCode: String) {
